a9b3be0b07e580b2935c03553e26add73e82f7f6
Ivan <saas@gmail.com>
2025-07-10 19:25:22 +0700

    neww

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -5,7 +5,7 @@ import React, {useState} from "react";
 
 import {Swiper, SwiperSlide, } from "swiper/react";
 import "swiper/css";
-
+import type { Swiper } from "swiper";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
