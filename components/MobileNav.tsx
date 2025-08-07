5753748e3493558741e48ef03b71fe9458e494fb
Ivan <ivanseptaa@gmail.com>
2025-08-07 17:36:08 +0700

    update

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -10,7 +10,7 @@ const links = [
   { name: "Services", path: "/services" },
   { name: "Resume", path: "/resume" },
   { name: "Work", path: "/work" },
-  { name: "Contact", path: "/contact" },
+  // { name: "Contact", path: "/contact" },
 ];
 
 const MobileNav = () => {
